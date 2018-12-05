9f6d409df70fa136a305f5032ae8c345ebc11f99
baiyang123123 <17635700990@163.com>
2018-12-05 16:11:12 +0800

    123546

--- bwgxgc/application/database.php
+++ bwgxgc/application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '192.168.1.6',
     // 数据库名
     'database'        => 'bwcm2018',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'root2',
     // 密码
-    'password'        => 'root',
+    'password'        => '123456',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
