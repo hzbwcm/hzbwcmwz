b8183359e41efc98388ef93345de9cee515aea62
monai <995275601@qq.com>
2019-05-08 16:28:17 +0800

    qwe

--- bwgxgc/application/home/controller/JghqController.php
+++ bwgxgc/application/home/controller/JghqController.php
@@ -3,9 +3,17 @@
 namespace app\home\controller;
 
 use think\Controller;
+use app\home\model\Banner;
+
 class JghqController extends Controller
 {
     public function jghq(){
+        $data = json(Banner::getBanner());
+        $this->assign('data',$data);
+
         return $this->fetch();
+
+
+        
     }
 }
